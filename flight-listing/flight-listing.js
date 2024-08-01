ffae6dccb382c540a0e2c5f7fd2e18283eb952cd
Sohail Asghar <sohailasghar90@gmail.com>
2024-08-02 02:16:08 +0500

    Bootswatch Theme and Design Fixes

--- flight-listing/flight-listing.js
+++ flight-listing/flight-listing.js
@@ -1,7 +1,7 @@
 function flightDetails (obj, new_color ) {
     console.log(obj);
-    let parrentElement = obj.closest(".flight-list-card");
-    let childBottomElement = parrentElement.querySelector('.flight-list-card-bottom')
+    let parrentElement = obj.closest(".flight-list");
+    let childBottomElement = parrentElement.querySelector('.flight-card-btm')
     if (childBottomElement.classList.contains("d-none")) {
         childBottomElement.classList.remove("d-none");
     } else {
